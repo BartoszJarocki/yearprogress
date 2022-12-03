2e063d7ece4484902e27f458c682557f72d3a5a3
Bartosz Jarocki <jarocki.bartek@gmail.com>
2022-12-03 14:34:55 +0100

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Space_Grotesk } from "@next/font/google";
+
+const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${spaceGrotesk.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ pages/api/og.tsx
@@ -0,0 +1,48 @@
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
+
+export const config = {
+  runtime: "experimental-edge",
+};
+
+const font = fetch(new URL('../../assets/SpaceGrotesk-Bold.ttf', import.meta.url)).then(
+  (res) => res.arrayBuffer(),
+);
+
+const generateOpenGraphImageHandler = async (req: NextRequest) => {
+  const { searchParams } = new URL(req.url);
+  const percentPassed = searchParams.get("percentPassed");
+  const currentYear = searchParams.get("currentYear");
+
+  const fontData = await font;
+
+  return new ImageResponse(
+    (
+      <div tw="h-screen w-screen flex bg-[#1f1f1f] text-[#e3e3e3]" style={{ fontFamily: "'Space Grotesk'"}}>
+        <div tw="m-auto flex flex-col items-center justify-center space-y-8 w-full max-w-2xl p-4">
+          <h1 tw="font-black text-6xl">{currentYear}</h1>
+          <div tw="flex h-8 w-full border border-[#e3e3e3]">
+            <div
+              tw="bg-gray-300 h-full"
+              style={{ width: `${percentPassed}%` }}
+            />
+          </div>
+          <h2 tw="font-extrabold text-4xl">{`${percentPassed}%`}</h2>
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: 'Typewriter',
+          data: fontData,
+          style: 'normal',
+        },
+      ],
+    }
+  );
+};
+
+export default generateOpenGraphImageHandler;
