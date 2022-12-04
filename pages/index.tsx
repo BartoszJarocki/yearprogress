26d3d7f3111d657f23828261f43cc7fae13eede1
Bartosz Jarocki <jarocki.bartek@gmail.com>
2022-12-04 15:46:30 +0100

    Added end of the year countdown

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { GetServerSideProps, NextPage } from "next";
-import { DateTime } from "luxon";
+import { DateTime, Duration } from "luxon";
 import { useEffect, useMemo, useRef, useState } from "react";
 import { showFireworks } from "../lib/fireworks/showFireworks";
 import { calculateYearProgress, calculateYearTimeLeft } from "../lib/utils";
@@ -9,31 +9,32 @@ const TIMER_INTERVAL_MS = 1000; // 1s
 const IS_CLOSE_THRESHOLD = 30;
 
 interface Props {
-  initialPercentPassed: number;
   initialTimeLeft: number;
   ogUrl: string;
 }
 
-const YearProgress: NextPage<Props> = ({
-  initialPercentPassed,
-  initialTimeLeft,
-  ogUrl,
-}) => {
+const YearProgress: NextPage<Props> = ({ initialTimeLeft, ogUrl }) => {
   const timeZoneRef = useRef(Intl.DateTimeFormat().resolvedOptions().timeZone);
-  const [timeLeftInSeconds, setTimeLeftInSeconds] = useState(initialTimeLeft);
-  const [percentPassed, setPercentPassed] = useState(initialPercentPassed);
+  const [timeLeftDuration, setTimeLeftDuration] = useState<Duration>();
+  const currentYear = useMemo(() => DateTime.local().year, []);
+  const timeLeftInSeconds = useMemo(
+    () =>
+      timeLeftDuration
+        ? Math.floor(timeLeftDuration.as("seconds"))
+        : initialTimeLeft,
+    [timeLeftDuration, initialTimeLeft]
+  );
+  const percentPassed = useMemo(
+    () => calculateYearProgress(timeLeftInSeconds),
+    [timeLeftInSeconds]
+  );
   const messageToDisplay =
     timeLeftInSeconds === 0 ? "Happy new year!" : timeLeftInSeconds;
   const isCloseToEnd = timeLeftInSeconds <= IS_CLOSE_THRESHOLD;
-  const currentYear = useMemo(() => DateTime.local().year, []);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      const timeLeftDuration = calculateYearTimeLeft(timeZoneRef.current);
-      const timeLeftInSeconds = Math.floor(timeLeftDuration.as("seconds"));
-
-      setTimeLeftInSeconds(timeLeftInSeconds);
-      setPercentPassed(calculateYearProgress(timeLeftInSeconds));
+      setTimeLeftDuration(calculateYearTimeLeft(timeZoneRef.current));
     }, TIMER_INTERVAL_MS);
 
     return () => clearInterval(intervalId);
@@ -53,9 +54,9 @@ const YearProgress: NextPage<Props> = ({
         canonical={"https://getyearprogress.com"}
         openGraph={{
           url: ogUrl,
-          title: "Year progress",
-          description: `${percentPassed}% of ${currentYear} has passed.`,
-          images: [{ url: ogUrl, width: 1200, height: 500 }],
+          title: `${currentYear} year progress`,
+          description: `${percentPassed}%`,
+          images: [{ url: ogUrl, width: 1200, height: 630 }],
           site_name: "Year progress",
         }}
       />
@@ -68,15 +69,27 @@ const YearProgress: NextPage<Props> = ({
             </h1>
           </div>
         ) : (
-          <div className="m-auto flex flex-col items-center justify-center gap-8 w-full max-w-2xl p-4">
-            <h1 className="font-black text-6xl">{currentYear}</h1>
-            <div className="h-8 w-full border">
+          <div className="m-auto flex flex-col items-center justify-center gap-8 w-full h-full max-w-2xl p-4">
+            <h1 className="mt-auto font-black text-6xl">{currentYear}</h1>
+            <div className="h-8 w-full border flex-shrink-0">
               <div
                 className="bg-gray-300 h-full"
                 style={{ width: `${percentPassed}%` }}
               />
             </div>
             <h2 className="font-extrabold text-4xl">{`${percentPassed}%`}</h2>
+            <div className="mt-auto font-mono min-h-[32px] text-xs">
+              {timeLeftDuration && (
+                <span>
+                  {timeLeftDuration.months > 0 && `${timeLeftDuration.months} months, `} 
+                  {timeLeftDuration.days > 0 && `${timeLeftDuration.days} days, `}
+                  {timeLeftDuration.hours} hours,{" "}
+                  {timeLeftDuration.minutes} minutes,{" "}
+                  {Math.floor(timeLeftDuration.seconds)} seconds{" "}
+                  left
+                </span>
+              )}
+            </div>
           </div>
         )}
       </main>
@@ -96,11 +109,10 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({
 
   return {
     props: {
-      initialPercentPassed: percentPassed,
       initialTimeLeft: timeLeftinSeconds,
       ogUrl: `https://${req.headers.host}/api/og?currentYear=${
-        ogYear || DateTime.local().year
-      }&percentPassed=${ogPercent || percentPassed}`,
+        ogYear ?? DateTime.local().year
+      }&percentPassed=${ogPercent ?? percentPassed}`,
     },
   };
 };
