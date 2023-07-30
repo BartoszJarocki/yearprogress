d4790216775858b323fac4402d3e75ccdfdda1cb
Bartosz Jarocki <bartosz.jarocki@hey.com>
2023-07-30 11:22:15 +0200

    Updated next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Space_Grotesk } from "@next/font/google";
+import { Space_Grotesk } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
